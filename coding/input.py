5703a8da98d4ec9d273e19a266f34c7585ac5777
Abhijit Roy <dalvath3700@gmail.com>
2020-01-31 01:08:17 +0530

    readme modified

---
+++ coding/input.py
@@ -0,0 +1,10 @@
+file_in_1 = '../data/Antenna ratio updated.xlsx'
+file_in_2 = '../data/PID raw data updated (002).xlsx'
+
+# Input file 1
+col_1 = ['DUT', 'transistor type', 'ref correlation', 'ref', 'antenna ratio TS18']
+sht_name_1 = 'Sheet1'
+# Input file 2
+col_2 = ['Lot_Wafer', 'Module', 'DUT', 'Algorithm', 'UnitET', 'Site', 'SiteValueET']
+sht_name_2 = 'Sheet1'
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+DUT, transistor type, ref correlation, ref, antenna ratio TS18
+
+Lot_Wafer, Module, DUT, Algorithm, UnitET, Site, SiteValueET
+
+# output
+1. transistor type
+2. ref_correlation
+3. antenna ratio
+4. DUT 
